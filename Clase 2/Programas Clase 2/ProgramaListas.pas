6463917cdb377f683b136d832dd8790758b7095e
stefanolomo <stefanolomo@gmail.com>
2025-08-18 08:54:27 -0300

    Clase 2

---
+++ Clase 2/Programas Clase 2/ProgramaListas.pas
@@ -0,0 +1,43 @@
+Program listas;
+Type
+  Lista= ^Nodo;
+  Nodo= Record
+           Datos: integer;
+           Sig: Lista;
+        End;
+Var
+ L: Lista;
+ n: integer;
+
+Procedure AgregarAdelante (var L:lista; num:integer);
+Var nue:Lista;
+  Begin
+    New(nue);
+    nue^.datos:=num;
+    nue^.sig:=L;
+    L:=nue;
+  End;
+
+
+Procedure Imprimir (pri:lista);
+Begin
+   while (pri <> NIL) do begin
+     write (pri^.datos, ' ');
+     pri:= pri^.sig
+  end;
+  writeln;
+end;
+
+
+begin
+ L:=nil;
+ randomize;
+ n := random (100);
+ While (n<>0) do Begin
+   AgregarAdelante (L, n);
+   n := random (100);
+ End;
+ writeln ('Lista generada: ');
+ imprimir (L);
+ readln
+end.
---
+++ Clase 2/Programas Clase 2/ProgramaVectorOrdenado.pas
@@ -0,0 +1,69 @@
+program VectorOrdenado;
+
+const
+    dimF = 8;  {Dimensión física del vector}
+
+type
+
+    vector = array [1..dimF] of integer;
+
+    dim = 0..dimF;
+
+{-----------------------------------------------------------------------------
+CARGARVECTORORDENADO - Carga ordenadamente nros aleatorios entre 0 y 100 en el
+vector hasta que llegue el nro 99 o hasta que se complete el vector}
+
+Procedure cargarVectorOrdenado ( var vec: vector; var dimL: dim);
+var
+   d, pos, j: integer;
+begin
+    Randomize;  { Inicializa la secuencia de random a partir de una semilla}
+    dimL := 0;
+    d:= random(100);
+    while (d <> 99)  and ( dimL < dimF ) do begin
+       pos:= 1;
+       while (pos <= dimL) and (vec[pos]< d) do pos:= pos + 1;
+       for  j:= dimL downto pos do vec[j+1]:= vec[j] ;
+       vec[pos]:= d;
+       dimL := dimL + 1;
+       d:= random(100)
+     end;
+end;
+
+{-----------------------------------------------------------------------------
+IMPRIMIRVECTOR - Imprime todos los nros del vector }
+
+Procedure imprimirVector ( var vec: vector; var dimL: dim );
+var
+   i: dim;
+begin
+     for i:= 1 to dimL do
+         write ('-----');
+     writeln;
+     write (' ');
+     for i:= 1 to dimL do begin
+        if(vec[i] < 9)then
+            write ('0');
+        write(vec[i], ' | ');
+     end;
+     writeln;
+     for i:= 1 to dimL do
+         write ('-----');
+     writeln;
+     writeln;
+End;
+
+{PROGRAMA PRINCIPAL}
+var
+   v: vector;
+   dimL : dim;
+
+begin
+
+     cargarVectorOrdenado(v,dimL);
+
+     writeln('Nros almacenados: ');
+     imprimirVector(v, dimL);
+
+     readln;
+end.
