2fb5bef00f358fa17f08f25ba7fb98879ebf9592
stefanolomo <stefanolomo@gmail.com>
2025-08-18 09:02:51 -0300

    funcion factorial

--- Clase 2/Programas Clase 2/ProgramaCalculoDeFactorial.pas
+++ Clase 2/Programas Clase 2/ProgramaCalculoDeFactorial.pas
@@ -1,15 +1,28 @@
-program CalculoDeFactorial;
+Program CalculoDeFactorial;
+
+Function Factorial(n: Integer):   Int64;
+
+Var 
+    i:   Integer;
+    result:   Int64;
+Begin
+    If n < 0 Then
+        writeln('No hay factorial de nros negativos');
+    result := 1;
+    For i := 2 To n Do
+        result := result * i;
+    Factorial := result;
+End;
 
 
 {PROGRAMA PRINCIPAL}
-var 
-  num: integer;
+
+Var 
+    num:   integer;
 Begin
 
-  writeln( 'Ingrese un entero no negativo:');
-  readln (num);
+    writeln( 'Ingrese un entero no negativo:');
+    readln (num);
 
-  writeln ( num, '! = ', factorial(num));
-  readln;
+    writeln ( num, '! = ', factorial(num));
 End.
-
