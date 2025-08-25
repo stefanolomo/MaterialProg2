549360488dbd9dc092f227af2aac9141d8fdd77b
stefanolomo <stefanolomo@gmail.com>
2025-08-24 21:00:19 -0300

    Cambio de nombres, indentacion

--- Actividad colaborativa 1/program.pas
+++ Actividad colaborativa 1/program.pas
@@ -18,15 +18,15 @@ Type
         HD:   arbol;
     End;
 
-Procedure enOrden( A: arbol );
+Procedure InformarEnOrden( A: arbol );
 Begin
     If ( A <> Nil ) Then
         Begin
-            enOrden (a^.HI);
+            InformarEnOrden(a^.HI);
             Writeln('Nombre: ', A^.datos.nombre,
                     '  Distancia: ', A^.datos.distancia:0:2,
                     '  Ventas: ', A^.datos.ventas);
-            enOrden (a^.HD)
+            InformarEnOrden(a^.HD)
         End;
 End;
 
@@ -34,7 +34,7 @@ Procedure verMinRecorrido(A: arbol; Var valor: real);
 
 Begin
 
-// El minimo de un arbol binario ordenado siempre va a estar a la izquierda de todos los nodos, es decir, va a ser la hoja mas a la izquierda del arbol
+    // El minimo de un arbol binario ordenado siempre va a estar a la izquierda de todos los nodos, es decir, va a ser la hoja mas a la izquierda del arbol
 
     If (A = Nil) Then // El arbol esta vacio
         valor := -1
@@ -78,12 +78,13 @@ Begin
     writeln('');
 End;
 
-Procedure cargarDestino(Var a:arbol);
+Procedure LeerDestino(Var a :arbol);
 
-Var datos:   datarbol;
+Var
+    datos:   datarbol;
 
 Begin
-    write('inserte destino:');
+    write('Inserte destino:');
     readln(datos.nombre);
     write('inserte distancia:');
     readln(datos.distancia);
@@ -106,30 +107,32 @@ End;
 //Programa Principal
 
 Var
-
     a:   arbol;
     decision:   integer;
 
 Begin
     writeln('Bienvenido Al Programa De Gestion De Viajes en autobus');
-
+    Inicializar(a);
     decision := 999;
 
-    While (decision<>0) Do
+    While (decision <> 0) Do
         Begin
             writeln('Menu De Opciones:');
-            writeln(
-             '1-Inicializar Estructura De Datos (Necesario para funcionamiento)'
-            );
+            writeln('1-Inicializar Estructura De Datos (Necesario para funcionamiento)');
             writeln('2-Cargar Destinos');
             writeln('3-imprimir Informacion Contenida');
             writeln('4-Buscar Destino Y Mostrar Su Informacion');
             writeln('5-Buscar Destino Mas Cercano');
             writeln('6-Sumar Pasajes A Un Destino');
             writeln('0-Terminar');
-            writeln;
+
+            Separador();
+
             write('Inserte Opcion:');
             readln(decision);
+
+            Separador();
+
             While (decision<0)Or(decision>6) Do
                 Begin
                     writeln('Inserte Opcion Valida:');
