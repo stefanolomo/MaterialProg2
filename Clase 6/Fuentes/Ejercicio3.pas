55c52947eaba1d9dee524bd355a88fa8d6200944
stefanolomo <stefanolomo@gmail.com>
2025-09-01 10:57:34 -0300

    formato

--- Clase 6/Fuentes/Ejercicio3.pas
+++ Clase 6/Fuentes/Ejercicio3.pas
@@ -1,117 +1,126 @@
 Program ejercicio3;
-Uses
-     sysutils;
+
+Uses sysutils;
+
 Type
-     pedido= record
-	      codSeg: integer;
-	      fechaYhora: string;
-	      dni: integer;
-	      codArea: integer;
-          domicilio: string;
-          tel: string;
-     end;
+     pedido =   Record
+          codSeg:   longint;
+          fechaYhora:   string;
+          dni:   longint;
+          codArea:   longint;
+          domicilio:   string;
+          tel:   string;
+     End;
 
-     listaPedidos = ^nodoLista;
-     nodoLista = record
-               dato: pedido;
-               sig: listaPedidos;
-     end;
+    listaPedidos =   ^nodoLista;
+    nodoLista =   Record
+        dato:   pedido;
+        sig:   listaPedidos;
+    End;
 
-procedure agregarLista(var pri:listaPedidos; p:pedido);
-var
-  nuevo, anterior, actual: listaPedidos;
-begin
-    new (nuevo);
-    nuevo^.dato:= p;
-    nuevo^.sig := nil;
-    if (pri = nil) then
-         pri := nuevo
-    else
-    begin
-         actual := pri;
-         anterior := pri;
-         while (actual<>nil) and (actual^.dato.dni < nuevo^.dato.dni) do begin
-              anterior := actual;
-              actual:= actual^.sig;
-         end;
-         if (anterior = actual) then
-              pri := nuevo
-         else
-              anterior^.sig := nuevo;
-         nuevo^.sig := actual;
-    end;
-end;
+Procedure agregarLista(Var pri:listaPedidos;p:pedido);
+
+Var
+    nuevo,anterior,actual:   listaPedidos;
+Begin
+    new(nuevo);
+    nuevo^.dato := p;
+    nuevo^.sig := Nil;
+    If (pri=Nil)Then
+        pri := nuevo
+    Else
+        Begin
+            actual := pri;
+            anterior := pri;
+            While (actual<>Nil)And(actual^.dato.dni<nuevo^.dato.dni) Do
+                Begin
+                    anterior := actual;
+                    actual := actual^.sig;
+                End;
+            If (anterior=actual)Then
+                pri := nuevo
+            Else
+                anterior^.sig := nuevo;
+            nuevo^.sig := actual;
+        End;
+End;
 
 
-function cargarFecha(): string;{Genera una FECHA aleatoria}
-var
-  dia, mes, hora, seg: integer;
-  s: string;
-begin
-  dia := random(30)+1;
-  mes := random(12)+1;
-  s := Concat(IntToStr(dia),'/',IntToStr(mes),'/2020 - ');
-  hora := random(24);
-  seg := random(60);
-  if(hora < 10)then
-	s := Concat(s, '0', IntToStr(hora))
-  else
-	s:= Concat(s, IntToStr(hora));
-  if(seg < 10)then
-	s := Concat(s, ':0', IntToStr(seg))
-  else
-	s:= Concat(s,':', IntToStr(seg));
-  cargarFecha:= s;
-end;
+Function cargarFecha():   string;{GeneraunaFECHAaleatoria}
 
-procedure crearLista(var l: listaPedidos);
-var
-   cant, cod: integer;
-   p: pedido;
-begin
-     cant:= random(10); {genera hasta 100 elementos}
-     cod:= 1;
-     while (cant <> 0) do Begin
-          p.codSeg:= cod;  {codigo de seguimiento}
-          p.fechaYhora := cargarFecha();
-          p.dni := random(60000000);  {dni}
-          p.codArea := (random(4000)); {codigo de área}
-          p.domicilio:= Concat('Domicilio', IntToStr(cod)); {domicilio}
-          p.tel:= IntToStr(random(999999)+4000000); {telefono}
-          agregarLista(l, p);
-          cant:= cant-1;
-          cod := cod+1;
-     end;
-end;
+Var
+    dia,mes,hora,seg:   longint;
+    s:   string;
+Begin
+    dia := random(30)+1;
+    mes := random(12)+1;
+    s := Concat(IntToStr(dia),'/',IntToStr(mes),'/2020-');
+    hora := random(24);
+    seg := random(60);
+    If (hora<10)Then
+        s := Concat(s,'0',IntToStr(hora))
+    Else
+        s := Concat(s,IntToStr(hora));
+    If (seg<10)Then
+        s := Concat(s,':0',IntToStr(seg))
+    Else
+        s := Concat(s,':',IntToStr(seg));
+    cargarFecha := s;
+End;
 
-procedure imprimirPedido(p:pedido);
-begin
-    with(p)do
-         writeln('El pedido ',codSeg, ' del cliente ', dni, ' sera atendido en la fecha ', fechaYhora, ' en el codigo de area ', codArea, ' y domicilio ', domicilio, ' con tel. de contacto ', tel);
-end;
+Procedure crearLista(Var l:listaPedidos);
 
-procedure imprimirLista(l:listaPedidos);
-begin
-    while(l<>nil)do
-    begin
-         imprimirPedido(l^.dato);
-         l:=l^.sig;
-    end;
-end;
+Var
+    cant,cod:   longint;
+    p:   pedido;
+Begin
+    cant := random(10);{generahasta100elementos}
+    cod := 1;
+    While (cant<>0) Do
+        Begin
+            p.codSeg := cod;{codigodeseguimiento}
+            p.fechaYhora := cargarFecha();
+            p.dni := random(60000000);{dni}
+            p.codArea := (random(4000));{codigodeárea}
+            p.domicilio := Concat('Domicilio',IntToStr(cod));{domicilio}
+            p.tel := IntToStr(random(999999)+4000000);{telefono}
+            agregarLista(l,p);
+            cant := cant-1;
+            cod := cod+1;
+        End;
+End;
 
-var
-   l_inicial: listaPedidos;
+Procedure imprimirPedido(p:pedido);
+Begin
+    With (p) Do
+        writeln('El pedido: ',codSeg,', del cliente ',dni,
+                ' sera atendido en la fecha ',fechaYhora,
+                ' en el codigo de area ',codArea,' y domicilio ',domicilio,
+                ' con telefono de contacto ',tel);
+End;
 
-begin
-     Randomize;
-     {Se crea la estructura inicial}
-     l_inicial:= nil;
-     crearLista(l_inicial);
-     writeln ('Lista: ');
-     imprimirLista(l_inicial);
+Procedure imprimirLista(l:listaPedidos);
+Begin
+    While (l<>Nil) Do
+        Begin
+            imprimirPedido(l^.dato);
+            l := l^.sig;
+        End;
+End;
 
-     {Completar el programa}
+Var
+    l_inicial:   listaPedidos;
 
-     writeln('Fin del programa');
-     readln;
-end.
+Begin
+    Randomize;
+{Secrealaestructurainicial}
+    l_inicial := Nil;
+    crearLista(l_inicial);
+    writeln('Lista:');
+    imprimirLista(l_inicial);
+
+{Completarelprograma}
+
+    writeln('Fin del programa');
+    readln;
+End.
