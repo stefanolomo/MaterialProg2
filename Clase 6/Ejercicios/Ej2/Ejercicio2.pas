54442a74eb036d6ba073cd6a31ed4b0be2b68032
stefanolomo <stefanolomo@gmail.com>
2025-09-01 14:30:43 -0300

    tipos

--- Clase 6/Ejercicios/Ej2/Ejercicio2.pas
+++ Clase 6/Ejercicios/Ej2/Ejercicio2.pas
@@ -1,8 +1,6 @@
 
 Program Ejercicio2;
 
-
-
 // Una aerolínea quiere premiar a sus mejores clientes y para ello, lee la información de los pasajes que ha vendido. De cada venta se conoce un código de vuelo (alfanumérico, ej: AR1620), cantidad de millas recorridas, DNI del pasajero, su nombre y apellido y la clase en que solicitó el boleto. Esta información se lee ordenada por código de vuelo.
 
 // Se necesita saber qué cantidad de puntos acumularon los pasajeros de la siguiente manera: cada pasajero que haya viajado en clase ejecutiva suma 100 puntos por milla de vuelo, mientras que aquellos que viajaron en clase turista suman 25 puntos por milla. Por lo que se pide:
@@ -36,10 +34,11 @@ Type
     End;
 
     pasajero =   Record
-        nombre, apellido:   string;
-        millas, puntos, dni:   integer;
+        dni     :   integer;
+        nombre  :   string;
+        apellido:   string;
+        vuelos  :   listaVuelos;  { vuelos y sus puntos }
     End;
-
     arbol =   ^Nodo;
     Nodo =   Record
         datos:   pasajero;
