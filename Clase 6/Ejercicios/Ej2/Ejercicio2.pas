c9dc16157f8e12aee663bdb373757c119b0fd6f4
stefanolomo <stefanolomo@gmail.com>
2025-09-01 13:04:55 -0300

    Esqueleto ejercicio

--- Clase 6/Ejercicios/Ej2/Ejercicio2.pas
+++ Clase 6/Ejercicios/Ej2/Ejercicio2.pas
@@ -1,4 +1,8 @@
-program Ejercicio2;
+
+Program Ejercicio2;
+
+
+
 // Una aerolínea quiere premiar a sus mejores clientes y para ello, lee la información de los pasajes que ha vendido. De cada venta se conoce un código de vuelo (alfanumérico, ej: AR1620), cantidad de millas recorridas, DNI del pasajero, su nombre y apellido y la clase en que solicitó el boleto. Esta información se lee ordenada por código de vuelo.
 
 // Se necesita saber qué cantidad de puntos acumularon los pasajeros de la siguiente manera: cada pasajero que haya viajado en clase ejecutiva suma 100 puntos por milla de vuelo, mientras que aquellos que viajaron en clase turista suman 25 puntos por milla. Por lo que se pide:
@@ -13,8 +17,68 @@ program Ejercicio2;
 
 // D. Realizar un programa que simule el llamado a los módulos b) y c), e informe lo devuelto por c).
 
-type
+Type
+    venta =   Record
+        codigo, nombre, apellido:   string;
+        millas, dni:   integer;
+        clase:   integer;
+    End;
 
+    vuelo =   Record
+        codigo:   string;
+        puntos:   integer;
+    End;
 
-begin
-end.
+    listaVuelos =   ^nodoVuelos;
+    nodoVuelos =   Record
+        datos:   vuelo;
+        sig:   listaVuelos;
+    End;
+
+    pasajero =   Record
+        nombre, apellido:   string;
+        millas, puntos, dni:   integer;
+    End;
+
+    arbol =   ^Nodo;
+    Nodo =   Record
+        datos:   pasajero;
+        HI:   arbol;
+        HD:   arbol;
+    End;
+
+Procedure leerVuelos(Var vuelos: listaVuelos);
+
+Begin
+    // Lee los vuelos y los almacena en la lista
+End;
+
+Procedure actualizarArbol(Var arbol: arbol; vuelos: listaVuelos);
+
+Begin
+    // Actualiza el árbol con los vuelos
+End;
+
+Function calcularSumaPuntos(arbol: arbol):   integer;
+
+Begin
+    // Calcula la suma de puntos de cada nodo y encuentra el máximo
+End;
+
+Procedure imprimirMayorPuntaje(arbol: arbol);
+
+Begin
+    // Imprime el mayor puntaje de los pasajeros cuyo DNI se encuentre entre 40.000.000 y 50.000.000
+End;
+
+Var
+    vuelos:   listaVuelos;
+    arbol:   arbol;
+
+Begin
+    // Programa principal
+    leerVuelos(vuelos);
+    actualizarArbol(arbol, vuelos);
+    writeln('Mayor puntaje:', calcularSumaPuntos(arbol));
+    imprimirMayorPuntaje(arbol);
+End.
