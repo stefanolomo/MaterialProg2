bea0dda4d28cc7b8761ab6a775a0248e06f8c600
stefanolomo <stefanolomo@gmail.com>
2025-09-02 16:33:04 -0300

    Clarificacion de comentarios

--- Clase 4/ArbolesBusqueda.pas
+++ Clase 4/ArbolesBusqueda.pas
@@ -132,14 +132,16 @@ Procedure busquedaAcotada(a: arbol; inf:integer; sup:integer);
 
 Begin
     If (a <> Nil) Then
-        If (a^.dato >= inf) Then // Si el dato es mayor a la cota menor
-            If (a^.dato <= sup) Then Begin // y tambien menor a la cota mayor
+        If (a^.dato >= inf) Then // Si el dato es mayor o igual a la cota inferior
+            If (a^.dato <= sup) Then Begin // Y también menor o igual a la cota superior
                 write(a^.dato,' - '); // Imprimirlo
                 busquedaAcotada(a^.hi, inf, sup); // Seguir en su rama derecha
                 busquedaAcotada (a^.hd, inf, sup); // y seguir en su rama izquierda
-            End Else // El dato es mayor a la cota menor pero se pasa de la cota mayor, entonces seguir a la izquierda donde hay datos menores a el
+            End Else
+                // El dato es mayor o igual la cota menor pero se pasa de la cota superior, entonces seguir a la izquierda donde hay datos menores a el
                 busquedaAcotada(a^.hi, inf, sup)
-        Else // El dato es menor a la cota menor (a ambas) entonce seguir a la derecha donde hay datos mayores a el
+        Else
+        // El dato es menor a la cota inferior (a ambas) entonces seguir a la derecha donde hay datos mayores a el
         busquedaAcotada(a^.hd, inf, sup);
 End;
 
