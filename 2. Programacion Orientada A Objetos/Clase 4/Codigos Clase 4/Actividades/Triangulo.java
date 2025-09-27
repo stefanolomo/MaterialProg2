28272bc93147d4352dde1a5a9dabaac9f113e08b
stefanolomo <stefanolomo@gmail.com>
2025-09-27 17:41:04 -0300

    Constructor de equilateros

--- 2. Programacion Orientada A Objetos/Clase 4/Codigos Clase 4/Actividades/Triangulo.java
+++ 2. Programacion Orientada A Objetos/Clase 4/Codigos Clase 4/Actividades/Triangulo.java
@@ -21,11 +21,18 @@ public class Triangulo
     }
 
     public Triangulo(String colorLinea, String colorRelleno, double lado1, double lado2, double lado3) {
+        // Constructor full
+
         this.colorLinea = colorLinea;
         this.colorRelleno = colorRelleno;
         this.lado1 = lado1;
         this.lado2 = lado2;
         this.lado3 = lado3;
+    }
+
+    public Triangulo(String colorLinea, String colorRelleno, double lado) {
+        // Constructor equilateros
+        this(colorLinea, colorRelleno, lado, lado, lado);
     }
 
     /**
