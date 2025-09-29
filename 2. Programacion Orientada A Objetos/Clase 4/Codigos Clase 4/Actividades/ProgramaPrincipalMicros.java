a5bb7164cd0445c2b231ab0170413c86a2515177
stefanolomo <stefanolomo@gmail.com>
2025-09-28 21:46:31 -0300

    Modulo de carga interactiva

--- 2. Programacion Orientada A Objetos/Clase 4/Codigos Clase 4/Actividades/ProgramaPrincipalMicros.java
+++ 2. Programacion Orientada A Objetos/Clase 4/Codigos Clase 4/Actividades/ProgramaPrincipalMicros.java
@@ -21,7 +21,42 @@ public class ProgramaPrincipalMicros {
         
     }
     
+    public static void gestionarCargaInteractiva(Micro microACargar, Scanner in) {
+        System.out.println("--- Carga de Pasajeros para el Micro a " + microACargar.getDestino() + " ---");
         
-        MicroMarDelPlata = MicroMarDelPlata.leerMicro(in);
+        System.out.println("A continuacion, ingresar el número de asiento a ocupar (1-20). Ingrese -1 para finalizar.");
+        
+        int asientoLeido = 0;
+        
+        while (!microACargar.estaLleno() && asientoLeido != -1) {
+            System.out.print("Ingrese nro. de asiento: ");
+            asientoLeido = in.nextInt(); // Asiento leido en lenguaje coloquial
+            
+            if (asientoLeido != -1) {
+                int indiceAsiento = asientoLeido - 1; // Es el indice del asiento leido
+                
+                if (microACargar.esAsientoValido(indiceAsiento)) {
+                    if (microACargar.ocuparAsiento(indiceAsiento)) { // Si  se ocupa con exito se informa
+                        System.out.println("El asiento " + (asientoLeido) + " se ocupo exitosamente.");
+                    } else { // Si no, el asiento ya estaba ocupado, se informa igual
+                        System.out.println("El asiento " + (asientoLeido) + " ya estaba ocupado");
+                        
+                        int primerLibre = microACargar.primerDesocupado();
+                        // Si hay al menos un asiento libre siguiente, se sugiere usar ese
+                        if (primerLibre != -1) {
+                            System.out.println("El proximo asiento libre es: " + (primerLibre +  1) + ". Desea cargar al pasajero en ese asiento? [1: Si, 2: No]");
+                            int decision = in.nextInt();
+                            
+                            if (decision == 1) { // Si se elegie usar el siguiente asiento libre, se ocupa ese
+                                microACargar.ocuparAsiento(primerLibre);
+                                System.out.println("El asiento " + (primerLibre + 1) + " se ocupo exitosamente.");
+                            }
+                        }
+                    }
+                } else { // Si no ingreso un asiento valido, se informa
+                    System.out.println("El asiento ingresado no es valido! Debe estar entre 1 y 20");   
+                }
+            }
+        }
     }
 }
