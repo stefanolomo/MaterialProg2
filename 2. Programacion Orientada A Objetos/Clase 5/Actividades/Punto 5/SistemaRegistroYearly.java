2e04606d378009cb77952ea0a73607fb5225c06f
Stefano <65745574+stefanolomo@users.noreply.github.com>
2025-10-08 17:36:33 -0300

    pre (#20)

--- 2. Programacion Orientada A Objetos/Clase 5/Actividades/Punto 5/SistemaRegistroYearly.java
+++ 2. Programacion Orientada A Objetos/Clase 5/Actividades/Punto 5/SistemaRegistroYearly.java
@@ -1,8 +1,4 @@
 public class SistemaRegistroYearly extends SistemaRegistro {
-    // Variables de estado
-    
-    private double[][] temperaturas;
-    
     // Constructores
     
     public SistemaRegistroYearly () {}
@@ -14,14 +10,6 @@ public class SistemaRegistroYearly extends SistemaRegistro {
     
     // Getters y Setters
     
-    public void setTemperatura(int month, int year, double temperatura){
-        this.temperaturas[year - super.getInicio()][month - 1] = temperatura;
-    };
-    
-    public double getTemperatura(int month, int year) {
-        return this.temperaturas[year - super.getInicio()][month - 1];
-    };
-    
     public double getPromedioYearly(int year) {
         double suma = 0;
         
@@ -29,7 +17,7 @@ public class SistemaRegistroYearly extends SistemaRegistro {
             suma = suma + temperaturas[year][i];
         }
         
-        return suma / 12.0;     
+        return suma / 12.0;   
     };
     
     public String getReporte() {
